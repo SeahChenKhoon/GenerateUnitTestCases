8aecf5723af90be66370133b3ba7441de4dc54e8
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-11 17:41:51 +0800

    test: test

--- temp.py
+++ temp.py
@@ -3,6 +3,6 @@ import math
 from theory_evaluation.circle_utils import circle_area, circle_circumference
 
 def test_circle_area_positive_radius():
-    radius = 5
+    radius = 5.0
     expected_area = math.pi * radius ** 2
     assert circle_area(radius) == pytest.approx(expected_area)
