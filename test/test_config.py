cb90231dc86569548cddac14324486f50d4766b9
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-11 12:38:19 +0800

    test: test

--- test/test_config.py
+++ test/test_config.py
@@ -7,9 +7,3 @@ def test_settings_default_values():
     assert settings.API_NAME == "project_simulation_fastapi"
     assert settings.API_V1_STR == "/api/v1"
     assert settings.LOGGER_CONFIG_PATH == "../conf/base/logging.yml"
-
-def test_settings_custom_values():
-    custom_settings = Settings(API_NAME="custom_api", API_V1_STR="/custom/v1", LOGGER_CONFIG_PATH="/custom/path/logging.yml")
-    assert custom_settings.API_NAME == "custom_api"
-    assert custom_settings.API_V1_STR == "/custom/v1"
-    assert custom_settings.LOGGER_CONFIG_PATH == "/custom/path/logging.yml"
