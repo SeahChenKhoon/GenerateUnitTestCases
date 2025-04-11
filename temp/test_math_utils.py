32aba2b9c2d000a6516a673302a888d29169862b
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-11 12:03:06 +0800

    test: test

--- temp/test_math_utils.py
+++ temp/test_math_utils.py
@@ -7,6 +7,9 @@ def test_factorial_positive_integer():
 def test_factorial_zero():
     assert factorial(0) == 1
 
+def test_factorial_one():
+    assert factorial(1) == 1
+
 def test_factorial_negative_integer():
     with pytest.raises(ValueError):
         factorial(-1)
@@ -19,6 +22,9 @@ def test_is_prime_non_prime_number():
 
 def test_is_prime_one():
     assert is_prime(1) is False
+
+def test_is_prime_zero():
+    assert is_prime(0) is False
 
 def test_is_prime_negative_number():
     assert is_prime(-5) is False
