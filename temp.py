6e067f3b27aa8d023f3a8df03493ae1588d055e9
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-11 17:55:09 +0800

    test: test

--- temp.py
+++ temp.py
@@ -3,6 +3,5 @@ import math
 from theory_evaluation.circle_utils import circle_area, circle_circumference
 
 def test_circle_circumference_negative_radius():
-    radius = -5
     with pytest.raises(ValueError, match="Radius cannot be negative."):
-        circle_circumference(radius)
+        circle_circumference(-1)
