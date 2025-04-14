c2d596aeb6d06e58bff4c9940d2afe138d824dfe
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-14 15:44:56 +0800

    test: test

--- temp/temp.py
+++ temp/temp.py
@@ -3,8 +3,5 @@ from theory_evaluation.config import Settings
 from theory_evaluation import models
 
 
-def test_settings_default_values():
-    settings = Settings()
-    assert settings.API_NAME == "project_simulation_fastapi"
-    assert settings.API_V1_STR == "/api/v1"
-    assert settings.LOGGER_CONFIG_PATH == "../conf/base/logging.yml"
+def test_settings_instance():
+    assert isinstance(Settings(), BaseSettings)
