1aea885eb32923a8ba1f823f15ef1f938568361c
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-14 10:05:10 +0800

    test: test

--- temp.py
+++ temp.py
@@ -8,7 +8,6 @@ from sqlalchemy.exc import SQLAlchemyError, OperationalError
 from contextlib import contextmanager
 from . import models
 
-def test_delete_user_performance_not_exists(mock_session):
-    mock_db = MagicMock()
-    mock_session.return_value.__enter__.return_value = mock_db
-    mock_db.query().filter().all.return_value = []
+@patch("theory_evaluation.utils.get_db")
+def test_delete_user_performance_not_exists(mock_get_db, mock_session):
+    mock_get_db.return_value.__enter__.return_value = mock_session
