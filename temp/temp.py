612ed8bd46338230b7e238808c467255c13fcc43
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-14 10:11:28 +0800

    test: test

--- temp/temp.py
+++ temp/temp.py
@@ -8,6 +8,5 @@ from sqlalchemy.exc import SQLAlchemyError, OperationalError
 from contextlib import contextmanager
 from . import models
 
-def test_delete_user_performance_not_exists():
-    email = EmailStr("test@example.com")
-    question_id = UUID("12345678123456781234567812345678")
+def test_delete_user_performance_not_exists(mock_db_session):
+    mock_db_session.return_value.query.return_value.filter.return_value.all.return_value = []
