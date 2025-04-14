08fdef3789f3a0eec97b479264969189373ce91b
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-14 15:42:29 +0800

    test: test

--- temp/temp.py
+++ temp/temp.py
@@ -1,11 +1,10 @@
 from pydantic_settings import BaseSettings
 from theory_evaluation.config import Settings
+from theory_evaluation import models
 
-def test_global_settings_instance():
-    from theory_evaluation.config import SETTINGS
-    assert hasattr(SETTINGS, "API_NAME")
-    assert SETTINGS.API_NAME == "project_simulation_fastapi"
-    assert hasattr(SETTINGS, "API_V1_STR")
-    assert SETTINGS.API_V1_STR == "/api/v1"
-    assert hasattr(SETTINGS, "LOGGER_CONFIG_PATH")
-    assert SETTINGS.LOGGER_CONFIG_PATH == "../conf/base/logging.yml"
+
+def test_settings_default_values():
+    settings = Settings()
+    assert settings.API_NAME == "project_simulation_fastapi"
+    assert settings.API_V1_STR == "/api/v1"
+    assert settings.LOGGER_CONFIG_PATH == "../conf/base/logging.yml"
