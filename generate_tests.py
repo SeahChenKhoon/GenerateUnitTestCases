7578e4c3799da68abd67b26a1de044898cc76219
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-16 08:19:07 +0800

    test: test

--- generate_tests.py
+++ generate_tests.py
@@ -21,7 +21,7 @@ file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 
 # Read the verbosity flag
-VERBOSE_MODE = os.getenv("VERBOSE_MODE") == "1"
+VERBOSE_MODE = True
 print(f"os.getenv(VERBOSE_MODE) - {os.getenv("VERBOSE_MODE")}")
 # Configure logging accordingly
 logging.basicConfig(
